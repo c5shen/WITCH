5dcd767a369d11109d229a8a62091f86a505991a
Chengze Shen <zhazhashen@gmail.com>
2022-01-21 03:37:50 -0600

    updated example codes

--- examples/run.sh
+++ examples/run.sh
@@ -8,6 +8,6 @@ cd ../
 ##              using 10 HMMs to align a sequence
 #python3 witch.py -i examples/data/unaligned_all.txt -d scenarioB_output -o aligned.txt -w 0 -k 10
 
-## 3) Scenario C - backbone alignment available; backbone tree missing;
-##                 query sequences available 
-#python3 witch.py -b examples/data/backbone.aln.fasta -q examples/data/unaligned_frag.txt -d scenarioC_output -o aligned.txt
+# 3) Scenario C - backbone alignment available; backbone tree missing;
+#                 query sequences available 
+python3 witch.py -b examples/data/backbone.aln.fasta -q examples/data/unaligned_frag.txt -d scenarioC_output -o aligned.txt
