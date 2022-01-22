e3665d2d685f6f1cba9624161f72b76ede3fda3b
Chengze Shen <zhazhashen@gmail.com>
2022-01-22 13:31:40 -0600

    slightly changed example format; added backbone tree for the example

--- examples/run.sh
+++ examples/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-cd ../
 
 ## Scenario A - unaligned sequences only
-#python3 witch.py -i examples/data/unaligned_all.txt -d scenarioA_output -o aligned.txt
+#python3 ../witch.py -i data/unaligned_all.txt -d scenarioA_output -o aligned.txt
 
 ## Scenario B - unaligned sequences only; using bit scores;
 ##              using 10 HMMs to align a sequence
-#python3 witch.py -i examples/data/unaligned_all.txt -d scenarioB_output -o aligned.txt -w 0 -k 10
+#python3 ../witch.py -i data/unaligned_all.txt -d scenarioB_output -o aligned.txt -w 0 -k 10
 
 # 3) Scenario C - backbone alignment available; backbone tree missing;
 #                 query sequences available 
-python3 witch.py -b examples/data/backbone.aln.fasta -q examples/data/unaligned_frag.txt -d scenarioC_output -o aligned.txt
+python3 ../witch.py --num-cpus 6 -b data/backbone.aln.fasta -q data/unaligned_frag.txt -d scenarioC_output -o aligned.txt
